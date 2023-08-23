6b614a2d403ab195d0337a0f2d41f3f3cb32330a
Sunderland93 <samoilov.lex@gmail.com>
2023-08-24 00:09:50 +0400

    Add River modules to Waybar

--- usr/share/river/scripts/nmtui.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-HASH="$(echo "$@" | shasum | cut -f1 -d" " | cut -c1-7)"
-term_float="alacritty --class floating_shell -e"
-FLOCK="flock --verbose -n "$HOME/.local/state/${HASH}.lock""
-$FLOCK $term_float nmtui connect
