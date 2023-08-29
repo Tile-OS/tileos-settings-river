3b6f76780532a5e2e74660b3851837a37a0df65d
Sunderland93 <samoilov.lex@gmail.com>
2023-08-29 23:07:00 +0400

    Set Hack font for Rofi

--- etc/skel/.config/river/keybindings.sh
+++ etc/skel/.config/river/keybindings.sh
@@ -7,7 +7,7 @@ mod="Mod4"
 term="alacritty"
 
 # Application launcher
-launcher="rofi -show combi -combi-modi 'drun,run' -terminal $term -ssh-command '{terminal} {ssh-client} {host} [-p {port}]' -run-shell-command '{terminal} {cmd}' -show-icons"
+launcher="rofi -show combi -combi-modi 'drun,run' -terminal $term -ssh-command '{terminal} {ssh-client} {host} [-p {port}]' -run-shell-command '{terminal} {cmd}' -show-icons -font 'Hack 12'"
 
 # Volume changing notify
 volume_bar="/usr/share/river/scripts/volume-notify.sh"
