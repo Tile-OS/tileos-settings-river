c61fac8781254fd4d4c183ebed7a97f3be57fda0
Sunderland93 <samoilov.lex@gmail.com>
2023-10-27 17:18:18 +0400

    Fix clipboard manager

--- usr/share/river/scripts/clipboard-manager.sh
+++ usr/share/river/scripts/clipboard-manager.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 case "$1" in
-    --list) cliphist list | fuzzel --dmenu -font -p "Select item to copy:" --lines 10 --width 60 | cliphist decode | wl-copy;;
-    --del) cliphist list | fuzzel --dmenu -font -p "Select item to delete:" --lines 10 --width 60 | cliphist delete;;
+    --list) cliphist list | rofi -dmenu -p "Select item to copy:" -lines 10 -width 35 | cliphist decode | wl-copy;;
+    --del) cliphist list | rofi -dmenu -p "Select item to delete:" -lines 10 -width 35 | cliphist delete;;
 esac
